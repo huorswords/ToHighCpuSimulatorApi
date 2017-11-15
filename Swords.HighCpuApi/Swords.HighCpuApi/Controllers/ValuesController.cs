a2b670003d78547119a756d89faccbbc47da34a4
Angel G <agarcia@pasiona.com>
2017-11-15 09:49:50 +0100

    Initial demo code implementation

---
+++ Swords.HighCpuApi/Swords.HighCpuApi/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swords.HighCpuApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            this.FindPrimeNumber(id);
+            return "Done!";
+        }
+
+        private long FindPrimeNumber(int n)
+        {
+            int count = 0;
+            long a = 2;
+            while (count < n)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            return (--a);
+        }
+    }
+}
